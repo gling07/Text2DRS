be8bffa5003c838a0f6ea498715bdcf46f73e550
Gang Ling <linggang7@gmail.com>
2017-10-02 02:56:48 -0500

    fix issue to call lth; add semlink; add lthOutputs

--- main.py
+++ main.py
@@ -1,23 +1,54 @@
 import subprocess
+import os
 
-lth ='/home/gangling/Documents/ALM/lth_srl/'
+Text2DRS = os.getcwd()
 
-model = lth + 'models/penn_00_18_split_dict.model'
+#lth ='/home/gangling/Documents/ALM/lth_srl/'
+lth = '/Users/gling/Documents/ALM/lth_srl/'
 
-dict =lth + 'v_n_a.txt'
+os.chdir(lth)
+
+model = 'models/penn_00_18_split_dict.model'
+
+dict = 'v_n_a.txt'
 
 mem ='100M'
 
-cp ='{0}jars/lthsrl.jar:' \
-    '{1}jars/utilities.jar:' \
-    '{2}jars/trove.jar:' \
-    '{3}jars/seqlabeler.jar'.format(lth,lth,lth,lth)
+cp ='jars/lthsrl.jar:jars/utilities.jar:jars/trove.jar:jars/seqlabeler.jar'
 
-targetFile = '<' + lth + 'TestPool/SingleSupportingFact.txt>'
-outputFile = lth + 'TestOutput/test.tokens'
+targetFile = '<TestPool/SingleSupportingFact.txt>'
+outputTokens = 'TestOutput/SingleSupportingFact.tokens'
+inputTokens = '<' + outputTokens + '>'
+outputFile = Text2DRS + '/lthOutputs/SingleSupportingFact.txt'
 
 
-cmd = 'java -Xmx{0} -cp {1} se.lth.cs.nlp.depsrl.Preprocessor -allLTH {2} {3} {4} {5}'.format(mem,cp,model,dict,targetFile,outputFile)
+cmd = 'java -Xmx{0} -cp {1} se.lth.cs.nlp.depsrl.Preprocessor -allLTH {2} {3} {4} {5}'.format(mem,cp,model,dict,targetFile,outputTokens)
 
 subprocess.call(cmd,shell=True)
+
+synmodel = 'models/train_at_pp_more2nd.model'
+LM = 'models/lm_080602_uknpreds.model'
+GM_CD = 'models/global_partcq_mc_cd_2o_ukp.model'
+GM_CL = 'models/part12345_cq_mc_2o_ukp.sv.model'
+
+CP= 'jars/lthsrl.jar:jars/utilities.jar:jars/trove.jar'
+
+MEM= '2600M'
+
+NSYN = '4'
+NSEM = '4'
+
+SYNW = '25'
+GMW = '3'
+
+FORCE_VARGS = 'false'
+
+cmd2 = 'java -Xmx{0} -cp {1} se.lth.cs.nlp.depsrl.Main -runFull ' \
+       '{2} {3} {4} {5} pb_frames nb_frames {6} {7} {8} {9} ' \
+       'false false ' \
+       '{10} {11} {12}'.format(MEM,CP,LM,GM_CD,GM_CL,synmodel,NSYN,NSEM,SYNW,GMW,FORCE_VARGS,inputTokens,outputFile)
+
+subprocess.call(cmd2,shell=True)
+
+os.chdir(Text2DRS)
 
